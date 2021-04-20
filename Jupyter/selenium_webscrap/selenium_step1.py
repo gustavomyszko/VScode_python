52fc4b0907db9be63fc543851f2e0ff57fd451b6
gustavomyszko <gustavomyszko@gmail.com>
2021-04-20 18:30:02 -0300

    selenium_scraptest

---
+++ Jupyter/selenium_webscrap/selenium_step1.py
@@ -0,0 +1,12 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+driver = webdriver.Chrome()
+
+#looking for bugs.
+
+#webdriver = webdriver
+#options = Options
+#print(options)
+#print (webdriver.__version__)
