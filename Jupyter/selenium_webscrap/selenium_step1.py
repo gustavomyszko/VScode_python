b98bb2d65f5eae2d815b0dd8dcd740d55fd7883d
gustavomyszko <gustavomyszko@gmail.com>
2021-04-20 21:30:36 -0300

    text_only

--- Jupyter/selenium_webscrap/selenium_step1.py
+++ Jupyter/selenium_webscrap/selenium_step1.py
@@ -2,7 +2,7 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome("diretório do webdriver atual!!! se não buga,pai!")
 
 #looking for bugs.
 
